f4589b0ef205757897099863c9a14987975149c3
Eylaine <46253285@163.com>
2018-04-16 18:47:54 +0800

    优化http请求

--- utils/httputil.py
+++ utils/httputil.py
@@ -6,6 +6,7 @@
 import requests
 from utils.logutil import logger
 from utils.confutil import ConfUtil
+import json
 
 
 class HttpUtil(object):
@@ -18,12 +19,24 @@ class HttpUtil(object):
         self.headers = {"Authorization": self.config.getproperty("github", "token")}
 
     def get(self, path, **kwargs):
-        logger.info("发送get请求")
-        return self.request_session.get(url=self.url + path, headers=self.headers, **kwargs)
+        logger.info("发送不带参数的get请求，无headers")
+        return self.request_session.get(url=self.url + path, **kwargs)
+
+    def get(self, path, params=None, **kwargs):
+        logger.info("发送带参数get请求，无headers")
+        if params is None:
+            return self.request_session.get(path)
+        return self.request_session.get(url=self.url + path, parmas=params, **kwargs)
 
     def post(self, path, **kwargs):
-        logger.info("发送post请求")
+        logger.info("发送无参post请求")
         return self.request_session.post(url=self.url + path, headers=self.headers, **kwargs)
+
+    def post(self, path, data=None, **kwargs):
+        logger.info("发送post请求")
+        if data is None:
+            return self.request_session.post(path)
+        return self.request_session.post(url=self.url + path, data=json.dumps(data), **kwargs)
 
 
 httputil = HttpUtil()
