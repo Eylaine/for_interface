dfaf95843949ed25c27c66f70d574fd6f87fad07
Eylaine <46253285@163.com>
2018-05-07 20:21:06 +0800

    add

--- others/__init__.py
+++
@@ -1,4 +0,0 @@
-# coding=utf-8
-# Time    : 2018-04-21
-# Author  : Eylaine
-# File    : __init__.py.py
--- others/string.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-# Time    : 2018-04-22
-# Author  : Eylaine
-# File    : string.py
-
-str1 = "Hello, {0}"
-str2 = str1.format("world!")
-print(str1)
-print(str2)
