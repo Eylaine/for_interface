48ae535f4de7ef9d127e000c58924491f5a723f6
zhangzhonglin <zhangzhonglin@aikucun.com>
2019-07-09 19:54:27 +0800

    add

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,7 @@
 # _*_ coding: utf-8 _*_
 # @Time    : 2018/4/11
 # @Author  : zhonglin.zhang
-<<<<<<< HEAD
-=======
 
 from utils.file import get_yaml_value
 
 DOMAIN = get_yaml_value("$.url")
->>>>>>> 1654c58
